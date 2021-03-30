0d2aa63445b9dded8984e568129a73edfedd326c
Quoc Huy <quochuy270800@gmail.com>
2021-03-30 10:26:21 +0300

    first commit

---
+++ app/src/main/java/square/ball/group_finder/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package square.ball.group_finder.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
