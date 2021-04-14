cb86ab92b0e02a26ac9cd4b102ea932717f28170
Quoc Huy <quochuy270800@gmail.com>
2021-04-14 16:50:55 +0300

    calendar testing version

--- app/src/main/java/square/ball/group_finder/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/square/ball/group_finder/ui/calendar/CalendarFragment.kt
@@ -1,25 +1,41 @@
 package square.ball.group_finder.ui.calendar
 
+import android.content.Intent
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.CalendarContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+import androidx.lifecycle.ViewModelProvider
 import square.ball.group_finder.R
+import square.ball.group_finder.activities.MainActivity
+
+
 
 class CalendarFragment : Fragment() {
 
 
-
     private lateinit var calendarViewModel: CalendarViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
 
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        calendarViewModel =
+                ViewModelProvider(this).get(CalendarViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_calendar, container, false)
+//        val textView: TextView = root.findViewById(R.id.text_search)
+
+
+        return root
     }
+
 
 
 
