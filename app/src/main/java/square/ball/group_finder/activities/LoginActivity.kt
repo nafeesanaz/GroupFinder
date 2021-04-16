8b06679f9a920abbba42233036d363edd6e226a5
quyentrankhanhha <t9qutr00@students.oamk.fi>
2021-04-16 19:19:31 +0700

    logout func

--- app/src/main/java/square/ball/group_finder/activities/LoginActivity.kt
+++ app/src/main/java/square/ball/group_finder/activities/LoginActivity.kt
@@ -4,13 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 import square.ball.group_finder.R
 
 class LoginActivity : AppCompatActivity() {
 
-    private  lateinit var mAuth: FirebaseAuth
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +20,25 @@ class LoginActivity : AppCompatActivity() {
 
         mAuth = FirebaseAuth.getInstance()
 
+        val currentUser = mAuth.currentUser
+        if (currentUser != null) {
+            startActivity(Intent(this@LoginActivity, UserActivity::class.java))
+            finish()
+        }
+
+        login()
+
+        text_view_register.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, SignUpActivity::class.java))
+        }
+    }
+
+    private fun login() {
         login_btn.setOnClickListener {
             val email = email_login.text.toString().trim()
             val password = password_login.text.toString().trim()
 
-            if (email.isEmpty()){
+            if (email.isEmpty()) {
                 email_login.error = "Email Required"
                 email_login.requestFocus()
                 return@setOnClickListener
@@ -39,32 +55,17 @@ class LoginActivity : AppCompatActivity() {
                 password_login.requestFocus()
                 return@setOnClickListener
             }
-            
-            loginUser(email, password)
-            
-        }
 
-        text_view_register.setOnClickListener {
-            startActivity(Intent(this@LoginActivity, SignUpActivity::class.java))
-        }
-    }
-
-    private fun loginUser(email: String, password: String) {
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){ task ->
-            if (task.isSuccessful) {
-                login()
-            } else {
-                task.exception?.message?.let {
-                    toast(it)
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        startActivity(Intent(this@LoginActivity, UserActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(this@LoginActivity, "Login failed, please try again", Toast.LENGTH_LONG).show()
+                    }
                 }
-            }
-        }
-    }
 
-    override fun onStart(){
-        super.onStart()
-        mAuth.currentUser?.let {
-            login()
         }
     }
 }
