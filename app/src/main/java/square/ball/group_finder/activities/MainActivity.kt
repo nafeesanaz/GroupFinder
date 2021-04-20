06c84b7d0ca493296d5f97983b0fc25c9f7d9ca3
Quoc Huy <quochuy270800@gmail.com>
2021-04-20 09:13:56 +0300

    fix some problems

--- app/src/main/java/square/ball/group_finder/activities/MainActivity.kt
+++ app/src/main/java/square/ball/group_finder/activities/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_search, R.id.navigation_calendar, R.id.navigation_user))
 
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
     }
 }
