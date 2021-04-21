65418ac02e1babfb18cde6f50d04beb39e62a0bb
quyentrankhanhha <t9qutr00@students.oamk.fi>
2021-04-21 17:15:07 +0700

    fix

--- app/src/main/java/square/ball/group_finder/activities/SignUpActivity.kt
+++ app/src/main/java/square/ball/group_finder/activities/SignUpActivity.kt
@@ -3,7 +3,7 @@ package square.ball.group_finder.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
+
 import android.util.Patterns
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -29,39 +29,39 @@ class SignUpActivity : AppCompatActivity() {
             startActivity(Intent(this@SignUpActivity, LoginActivity::class.java))
         }
     }
-     private fun register() {
-            register_btn.setOnClickListener {
-                val email = email_register.text.toString().trim()
-                val password = password_register.text.toString().trim()
+    private fun register() {
+        register_btn.setOnClickListener {
+            val email = email_register.text.toString().trim()
+            val password = password_register.text.toString().trim()
 
 
-                if (email.isEmpty()) {
-                    email_register.error = "Email Required"
-                    email_register.requestFocus()
-                    return@setOnClickListener
-                }
+            if (email.isEmpty()) {
+                email_register.error = "Email Required"
+                email_register.requestFocus()
+                return@setOnClickListener
+            }
 
-                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    email_register.error = "Valid Email Required"
-                    email_register.requestFocus()
-                    return@setOnClickListener
-                }
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                email_register.error = "Valid Email Required"
+                email_register.requestFocus()
+                return@setOnClickListener
+            }
 
-                if (password.isEmpty() || password.length < 6) {
-                    password_register.error = "6 char password required"
-                    password_register.requestFocus()
-                    return@setOnClickListener
-                }
-                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
-                    if (it.isSuccessful) {
+            if (password.isEmpty() || password.length < 6) {
+                password_register.error = "6 char password required"
+                password_register.requestFocus()
+                return@setOnClickListener
+            }
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
 
-                        Toast.makeText(this@SignUpActivity, "Registration Success", Toast.LENGTH_LONG).show()
-                        finish()
-                    } else {
-                        Toast.makeText(this@SignUpActivity, "Registration failed, please try again", Toast.LENGTH_LONG).show()
-                    }
+                    Toast.makeText(this@SignUpActivity, "Registration Success", Toast.LENGTH_LONG).show()
+                    finish()
+                } else {
+                    Toast.makeText(this@SignUpActivity, "Registration failed, please try again", Toast.LENGTH_LONG).show()
                 }
             }
         }
+    }
 
 }
